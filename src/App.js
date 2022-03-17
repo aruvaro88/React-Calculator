80dac917e61c05fc3d1ed0881a047cd841ca89bc
aruvaro88 <aruvaro@gmail.com>
2022-03-17 21:41:53 +0100

    componentes y deteccion de botones

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import { Button } from "./components/Button/Button"
+import { ButtonContainer } from "./components/ButtonContainer/ButtonContainer"
+import { Container } from "./components/Container/Container"
+import { Screen } from "./components/Screen/Screen"
+
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0,".","="],
+]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container>
+          <Screen value="0" />
+          <ButtonContainer>
+            {
+              btnValues.flat().map((btn, i) => {
+                return (
+                  <Button
+                    key={i}
+                    className={btn === "=" ? "equals" : ""}
+                    value={btn}
+                    onClick={() => {
+                    console.log(`${btn} clicked`)
+                  }}/>
+                )
+              })
+            }
+          </ButtonContainer>
+        </Container>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/ButtonContainer/ButtonContainer.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./ButtonContainer.css"
+
+export const ButtonContainer = ({ children }) => {
+    return <div className="buttonContainer">{children}</div>
+}
