55b2ead903099f4179fced3a040468ee2a192d54
aruvaro88 <aruvaro@gmail.com>
2022-03-19 11:19:08 +0100

    calculator all working

--- src/App.js
+++ src/App.js
@@ -12,6 +12,10 @@ const btnValues = [
   [0, ".", "="],
 ]
 
+const toLocaleString = (num) => String(num).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, "$1 ")
+
+const removeSpaces = (num) => num.toString().replace(/\s/g, "")
+
 const App = () => {
   let [calc, setCalc] = useState({
     sign: "",
@@ -21,37 +25,87 @@ const App = () => {
 
   const numClickHandler = (e) => {
     e.preventDefault()
-    console.log("numclickhandler")
+    const value = e.target.innerHTML
+    if (removeSpaces(calc.num).length < 16) {
+      setCalc({
+        ...calc,
+        num:
+          calc.num === 0 && value === "0"
+            ? "0"
+            : removeSpaces(calc.num) % 1 === 0
+            ? toLocaleString(Number(removeSpaces(calc.num + value)))
+            : toLocaleString(calc.num + value),
+        res: !calc.sign ? 0 : calc.res,
+      })
+    }
   }
 
   const resetClickHandler = (e) => {
-    e.preventDefault()
-    console.log("resetClickHandler")
+    setCalc({
+      ...calc,
+      sign: "",
+      num: 0,
+      res: 0,
+    })
   }
 
   const invertClickHandler = (e) => {
-    e.preventDefault()
-    console.log("invertClickHandler")
+    setCalc({
+      ...calc,
+      num: calc.num ? toLocaleString(removeSpaces(calc.num) * -1) : 0,
+      res: calc.res ? toLocaleString(removeSpaces(calc.res) * -1) : 0,
+      sign: "",
+    })
   }
 
   const percentClickHandler = (e) => {
-    e.preventDefault()
-    console.log("percentClickHandler")
+    let num = calc.num ? parseFloat(removeSpaces(calc.num)) : 0
+    let res = calc.res ? parseFloat(removeSpaces(calc.res)) : 0
+
+    setCalc({
+      ...calc,
+      num: (num /= Math.pow(100.1)),
+      res: (res /= Math.pow(100, 1)),
+      sign: "",
+    })
   }
 
   const equalsClickHandler = (e) => {
-    e.preventDefault()
-    console.log("equalsClickHandler")
+    if (calc.sign && calc.num) {
+      const math = (a, b, sign) => (sign === "+" ? a + b : sign === "-" ? a - b : sign === "X" ? a * b : a / b)
+      setCalc({
+        ...calc,
+        res:
+          calc.num === "0" && calc.sign === "/"
+            ? "Cant divide with 0"
+            : toLocaleString(math(Number(removeSpaces(calc.res)), Number(removeSpaces(calc.num)), calc.sign)),
+        sign: "",
+        num: 0,
+      })
+      console.log(calc.res)
+    }
   }
 
   const signClickHandler = (e) => {
     e.preventDefault()
-    console.log("signClickHandler")
+    const value = e.target.innerHTML
+
+    setCalc({
+      ...calc,
+      sign: value,
+      res: !calc.res && calc.num ? calc.num : calc.res,
+      num: 0,
+    })
   }
 
   const decimalClickHandler = (e) => {
     e.preventDefault()
-    console.log("decimalClickHandler")
+    const value = e.target.innerHTML
+
+    setCalc({
+      ...calc,
+      num: !calc.num.toString().includes(".") ? calc.num + value : calc.num,
+    })
   }
 
   return (
@@ -64,7 +118,7 @@ const App = () => {
               return (
                 <Button
                   key={i}
-                  className={btn === "=" ? "equals" : ""}
+                  className={btn === "=" ? "equals" : "button"}
                   value={btn}
                   onClick={
                     btn === "C"
